c9217914839e80e8ca7eac66932dc89763767c13
Sayd <121144648+SaydRomey@users.noreply.github.com>
2023-09-25 11:52:01 -0400

    Update main_bonus.c

--- src_bonus/main_bonus.c
+++ src_bonus/main_bonus.c
@@ -18,14 +18,15 @@ Bonus version
 int	main(int argc, char **argv, char **envp)
 {
 	t_list	*path_lst;
+	int		valid_infile;
 
 	if (argc < 5)
 		return (ft_fprintf(FD_ERROR, USAGE_B), FAILURE);
-	validate_files(argv[1], argv[argc - 1]);
+	valid_infile = validate_files(argv[1], argv[argc - 1]);
 	path_lst = get_path_lst(envp);
 	if (!path_lst)
 		exit_error("Error: PATH not found in envp");
-	pipex(argc, argv, path_lst);
+	pipex(argc, argv, path_lst, valid_infile);
 	ft_lstclear(&path_lst, free);
 	return (SUCCESS);
 }
